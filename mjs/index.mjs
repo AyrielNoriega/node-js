5deb4cd84f8cc249be376a9d411ddd39ef0e1ed2
AyrielNoriega <anoriega2310@gmail.com>
2023-10-01 13:26:10 -0500

    la extension es obligatoria al momento de importar ??

--- mjs/index.mjs
+++ mjs/index.mjs
@@ -2,6 +2,6 @@
 // .mjs -> para utilizar ES Modules, y la forma recomendada al dia de hoy; Esto se puede configurar mejor en el package.json para que no sea necesario modificar la extension a .mjs, sino utilizar .js
 // .cjs -> para utilizar CommonJS
 
-import { sum } from './sum.mjs';
+import { sum } from './sum.mjs';   // en la especificacion de ECMASCRIPT MODULES las extencoines son obligatorias, al momento de importar; webpack o vite hace un poco de magia si no colocas la extension.
 
 console.log(sum(1, 2));
