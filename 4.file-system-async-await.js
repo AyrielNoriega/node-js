c717e1ef027dc666c4282d77bb52bfbf217ffea6
AyrielNoriega <anoriega2310@gmail.com>
2023-10-01 14:26:55 -0500

    lectura de archivo sincrona, asincrona y con promesas

--- 4.file-system-async-await.js
+++ 4.file-system-async-await.js
@@ -1,23 +1,5 @@
 // el file system es uno de los modulos mas importantes de nodejs, no paras de utilizarlo todos los dias.
 
-/*const fs = require('node:fs'); // a partir de Node 16, se recomienda poner node: antes de fs
-
-console.log('Leyendo primer archivo');
-fs.readFile('./archivo.txt', 'utf-8', (err, text) => {
-    console.log('texto del primer archivo: ', text);
-}); // readFile acepta como tercer parametro un callBack
-console.log('Cosas mientras lee primer archivo...');
-
-console.log('Leyendo segundo archivo..');
-fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
-    console.log('texto del segundo archivo: ',text);
-}); // readFile acepta como tercer parametro un callBack
-
-// esta es la forma asincrona de leear un archivo.
-*/
-
-// nosejs tiene una forma de hacer esto con promesas, de forma que ya no es necesario utilizar callbacks
-
 /*esto sólo en los módulos nativos que no tienen promesas nativas
 const {promisify} = require('node:util');
 const readFilePromise = promisify(fs.readFile);*/
